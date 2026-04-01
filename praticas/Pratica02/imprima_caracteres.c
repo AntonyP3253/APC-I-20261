094d44c4a42ee89fb6959e432f2be96fdbd5ac9e
Antony <antony.pelais@gmail.com>
2026-04-01 09:44:27 -0300

    feat: conclui Pratica02. Fecha #3

---
+++ praticas/Pratica02/imprima_caracteres.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    
+    char caractere = 'A';
+    char string[] = "Isso aqui eh uma string";
+    printf("%c\n", caractere);
+    printf("%s\n", string);
+
+    return 0;
+
+}
